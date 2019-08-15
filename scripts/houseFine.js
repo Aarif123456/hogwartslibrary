26f27952e377c0fd64a753e31f771fb7ce0f6864
Abdullah <abdullahmeo11@gmail.com>
2019-08-15 11:08:49 -0400

    add in basic chart

---
+++ scripts/houseFine.js
@@ -0,0 +1,40 @@
+//Abdullah Arif
+//Show cumulative fine for each house
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(houseJSON) {
+  var houseArray = [['Category', 'Number of Books in Category']];
+  var tmpArr;
+  for(var house in houseJSON  ){
+      tmpArr = [house.house,house.total];
+      houseArray.push(tmpArr);
+  }
+  var data = google.visualization.arrayToDataTable();
+
+  var options = {
+    title: 'Cumulative Fine of Hogwart houses'
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+//load after page loads
+window.addEventListener('DOMContentLoaded', (event) => {
+    //Ajax insert
+    var xmlhttp = new XMLHttpRequest();
+    var url="https://arif115.myweb.cs.uwindsor.ca/60334/projects/guestCharts";
+    var par = "chartType='houseFine'";
+    xmlhttp.open('POST', url, true);
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          drawChart(houseJSON);
+      }
+    }
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.withCredentials = true; 
+    xmlhttp.send(par); 
+
+});
+
