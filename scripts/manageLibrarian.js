0b0d0ae2a7a4616aae6b8d9f74804e5d05106465
Abdullah <abdullahmeo11@gmail.com>
2019-09-11 10:46:06 -0400

    fixed headmaster manage librarian page js file

--- scripts/manageLibrarian.js
+++ scripts/manageLibrarian.js
@@ -12,7 +12,7 @@ function modifyLibrarian(mode) {
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
       	console.log( this.responseText);
-        getUserList();
+        displayForm();
       }
     }
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
