5080e34da5e15a8f7c8f0aa076e8f164e4570c6f
Abdullah <abdullahmeo11@gmail.com>
2019-09-18 16:23:50 -0400

    added return

---
+++ scripts/return.js
@@ -0,0 +1,26 @@
+//Abdullah Arif
+//handles books being returned
+
+//potentially autofill for books
+//function suggestBarcode(barcode){}
+
+function returnBook() {
+    var bookBarcode =document.getElementById("bookBarcode").value;
+    var par ="bookBarcode="+bookBarcode;
+
+    //Ajax insert
+    var xmlhttp = new XMLHttpRequest();
+    var url="https://arif115.myweb.cs.uwindsor.ca/60334/projects/returnBook";
+    xmlhttp.open('POST', url, true);
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log( this.responseText); //**change div with id "hint"
+      }
+    }
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.withCredentials = true;
+    
+    xmlhttp.send(par); 
+    //clear fields
+    document.getElementById("return").reset();
+}
