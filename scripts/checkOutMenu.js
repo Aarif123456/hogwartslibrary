3c5adbf3cb5ef7c155f3d3d38b5d6f8e2595155c
Abdullah <abdullahmeo11@gmail.com>
2019-09-26 17:55:13 -0400

    made renew and lost based on checkboxes instead of buttons

--- scripts/checkOutMenu.js
+++ scripts/checkOutMenu.js
@@ -63,26 +63,43 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	function createCheckOut(transactionJSON){ //create table from json file
 	//var key= ""; //force make key an string
 	//create Table with given header
-	var tableText = "<table><thead><tr><td>Title</td><td>Author</td><td>Duedate</td><td>Renewed</td><td>Holds</td><td>Lost button</td><td>Renew button</td></tr></thead>";
+	var tableText = "<table><thead><tr>";
+	tableText+=`<td><input type="checkbox" id='renewAll' onclick=
+	"check_all('renewBoxes[]','renewAll' )"></td>`; //check box that lets you renew 
+	tableText+=`<td><input type="checkbox" id='lostAll' onclick=
+	"check_all('lostBoxes[]','lostAll')"></td>`;//check box for marking things as lost
+	tableText+=`<td>Title</td><td>Author</td><td>Duedate</td><td>Renewed</td><td>Holds</td>`;
+	tableText+=`</tr></thead>`;//end the head of the table
 	tableText+="<tbody>"; //Start table body 
 	var bookISBN ="";
 	for (var transaction of transactionJSON){
 		tableText += "<tr>"; //one row per transaction
+		tableText += `<td><input type="checkbox" name='renewBoxes[]' value ='`+bookBarcode+`'></td>`;
+		tableText += `<td><input type="checkbox" name='lostBoxes[]' value ='`+bookBarcode+`'></td>`;
 		tableText +="<td>"+ transaction['bookName'] +"</td>";
 		tableText +="<td>"+ transaction['author'] +"</td>";
-		tableText +="<td>"+ transaction['duedate'] +"</td>";
+		tableText +="<td>"+ transaction['dueDate'] +"</td>";
 		tableText +="<td>"+ transaction['renewedTime'] +"</td>";
 		tableText +="<td>"+ transaction['holds'] +"</td>";
 		
 		//console.log(transactionJSON[transaction]);
 		bookBarcode=transaction['bookBarcode'];
 		//console.log(bookISBN);
-		tableText +="<td>" + "<button type='submit' onclick='markLost(\""+bookBarcode+"\");'>Lost</button>" +"</td>";
-		tableText +="<td>" + "<button type='submit' onclick='renewBook(\""+bookBarcode+"\");'>Renew</button>" +"</td>";
 		tableText += "<tr>"; //end the row
 	}
 	tableText+= "</tbody> </table>";//close table
 	document.getElementById("checkout_table").innerHTML = tableText;	
+}
+
+function check_all(boxName,allboxname){
+	var checkedBoxes = document.getElementsByName(boxName); 
+	var i; 
+	if (document.getElementById(allboxname).checked) 
+		for (i = 0; i < checkedBoxes.length; i++) 
+			checkedBoxes[i].checked=true; 
+	else 
+		for (i = 0; i < checkedBoxes.length; i++) 
+			checkedBoxes[i].checked=false; 
 }
 
 /*for (var key in transactionJSON[transaction]){
