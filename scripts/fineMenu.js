5c524a16a337769aa26d9ca6559cdfe2555837d1
Abdullah <abdullahmeo11@gmail.com>
2019-09-27 15:45:42 -0400

    added more console.log to understand the cause of the bugs

--- scripts/fineMenu.js
+++ scripts/fineMenu.js
@@ -5,7 +5,7 @@ totalFine=-1;
 //Tells use how much they owe right now
 function displayTotalFine(finesJSON){
   var displayText="";
-  console.log(finesJSON);
+  console.log(finesJSON[0]);
   try{
     totalFine=parseInt(finesJSON.fines);
     var blacklisted=(parseInt(finesJSON.blacklisted)===1)?true:false; //get blacklist status of user
